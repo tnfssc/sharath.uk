c31e38d2ea5651cc3456ecbacca7009572793f60
Sharath <29162020+tnfssc@users.noreply.github.com>
2024-03-03 21:11:53 +0530

    🚑 removed router devtools in prod

--- src/components/__DEV__/TanStackRouterDevtools.tsx
+++ src/components/__DEV__/TanStackRouterDevtools.tsx
@@ -1,13 +1,15 @@
 import { Suspense, lazy } from 'react';
 
 const DevTools =
-  import.meta.env.NODE_ENV === 'production'
+  import.meta.env.DEV
     ? () => null
     : lazy(() =>
         import('@tanstack/router-devtools').then((res) => ({
           default: res.TanStackRouterDevtools,
         })),
       );
+
+console.log(import.meta.env.DEV);
 
 export const TanStackRouterDevtools = () => (
   <Suspense fallback={null}>
