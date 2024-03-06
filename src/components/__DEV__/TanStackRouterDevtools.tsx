e9375957bc27a34d4009f0791c6eb7b064753316
Sharath <29162020+tnfssc@users.noreply.github.com>
2024-03-06 11:09:32 +0530

    🚑 fix import.meta.env.DEV in TanStackRouterDevtools.tsx

--- src/components/__DEV__/TanStackRouterDevtools.tsx
+++ src/components/__DEV__/TanStackRouterDevtools.tsx
@@ -1,6 +1,6 @@
 import { Suspense, lazy } from 'react';
 
-const DevTools = import.meta.env.DEV
+const DevTools = !import.meta.env.DEV
   ? () => null
   : lazy(() =>
       import('@tanstack/router-devtools').then((res) => ({
