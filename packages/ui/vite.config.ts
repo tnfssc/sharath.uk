610458874b39f8b0df089907a4094380b0f37435
sharath <29162020+tnfssc@users.noreply.github.com>
2024-04-17 21:16:38 +0530

    chore(workspace): minor comment fix

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,7 +2,6 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import million from 'million/compiler';
-// @ts-expect-error
 import path from 'node:path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
@@ -13,7 +12,6 @@ export default defineConfig({
   plugins: [imagetools(), UnoCSS(), million.vite({ auto: true, telemetry: false }), react(), TanStackRouterVite()],
   resolve: {
     alias: {
-      // @ts-expect-error
       '@': path.resolve(__dirname, './src'),
     },
   },
