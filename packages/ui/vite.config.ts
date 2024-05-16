79ff463df162a6a257d90be4a9dfeda0b8ec436c
sharath <29162020+tnfssc@users.noreply.github.com>
2024-05-16 20:50:58 +0530

    chore: added build time env validation

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,15 +1,23 @@
 /// <reference types="vitest" />
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
-import million from 'million/compiler';
+import 'dotenv/config';
 import path from 'node:path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  VITE_API_ENDPOINT: z.string().url(),
+  VITE_PUBLIC_URL: z.string().url(),
+});
+
+envSchema.parse(process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [imagetools(), UnoCSS(), million.vite({ auto: true, telemetry: false }), react(), TanStackRouterVite()],
+  plugins: [imagetools(), UnoCSS(), react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
