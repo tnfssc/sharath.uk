bb8b0ccbaa95f0f861a13bc299bb06e6e5a2822b
sharath <29162020+tnfssc@users.noreply.github.com>
2024-06-29 19:27:28 +0530

    feat(about-writer): moved to new blog API

--- packages/ui/src/hooks/useAsset.ts
+++ packages/ui/src/hooks/useAsset.ts
@@ -16,7 +16,7 @@ export const useAsset = (url: string) => {
           resolve();
           return;
         }
-        fetch(url)
+        fetch(url, { referrerPolicy: 'no-referrer' })
           .then((r) => r.blob())
           .then((blob) => {
             // read to base64
